f486c51fbcc9728177694ba600b5f2ba0a7a5425
Allan Simon <simona@gobeta.com.cn>
2015-02-01 17:15:45 +0800

    remove unused route for the moment

--- src/SMG/OauthBundle/Controller/SecurityController.php
+++ src/SMG/OauthBundle/Controller/SecurityController.php
@@ -11,47 +11,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 class SecurityController extends Controller
 {
-    public function loginAction(Request $request)
-    {
-        $session = $request->getSession();
-
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } elseif (
-            null !== $session &&
-            $session->has(SecurityContext::AUTHENTICATION_ERROR)
-        ) {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
-            $error = null;
-        }
-
-        if ($error) {
-             // WARNING! Symfony source code identifies this line as
-             // a potential security threat.
-            $error = $error->getMessage();
-        }
-
-        $lastUsername = (null === $session) ?
-            '' :
-            $session->get(SecurityContext::LAST_USERNAME)
-        ;
-
-        return $this->render(
-            'SMGOauthBundle:Security:login.html.twig',
-            array(
-                'last_username' => $lastUsername,
-                'error' => $error,
-            )
-        );
-    }
-
-    public function loginCheckAction(Request $request)
-    {
-
-    }
-
     /**
      * Check if a token is valid
      * HTTP status 200 => valid
