3d0d8bea91faafbce11b6503c314bab0c1ea545f
Allan Simon <simona@gobeta.com.cn>
2015-05-17 12:43:58 +0800

    now only enabled user can log in

--- src/SMG/UserBundle/Manager/UserManager.php
+++ src/SMG/UserBundle/Manager/UserManager.php
@@ -9,13 +9,16 @@ class UserManager extends FOSUserManager
     public function loadUserByUsername($username)
     {
         $user = $this->findUserByUsernameOrEmail($username);
-        if (!is_null($user)) {
-            return $user;
+
+        if (is_null($user)) {
+            // if not username or email, we now try phone number
+            $phoneNumber = str_replace('+', '00', $username);
+            $user = $this->repository->findOneByPhoneNumber($phoneNumber);
         }
 
-        // if not username or email, we now try phone number
-        $phoneNumber = str_replace('+', '00', $username);
-        $user = $this->repository->findOneByPhoneNumber($phoneNumber);
+        if (is_null($user) || !$user->isEnabled()) {
+            return;
+        }
 
         return $user;
     }
